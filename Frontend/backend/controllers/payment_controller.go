5397357845061f18854800a83e7b7f7f9879a6ba
michał <51276321+michalzuch@users.noreply.github.com>
2024-04-23 16:14:51 +0200

    Frontend - create Products and Payment
    Project will be restructured, currently MVP
    
    - create Products and Payment components
    - setup axios
    - fetch data from backend app for Products
    - post data to backend app for Payment
    - improve website's UI
    - minor adjustments on the backend side

--- Frontend/backend/controllers/payment_controller.go
+++ Frontend/backend/controllers/payment_controller.go
@@ -8,25 +8,31 @@ import (
 
 func ProcessPayment(c echo.Context) error {
 	type PaymentRequest struct {
-		Amount           float32 `json:"amount"`
-		Name             string  `json:"name"`
-		CreditCardNumber string  `json:"credit_card_number"`
-		ExpirationDate   string  `json:"expiration_date"`
-		CVC              uint16  `json:"cvc"`
+		Name             string `json:"name"`
+		CreditCardNumber string `json:"credit_card_number"`
+		ExpirationDate   string `json:"expiration_date"`
+		CVC              uint16 `json:"cvc"`
 	}
 
 	var paymentReq PaymentRequest
+
 	if err := c.Bind(&paymentReq); err != nil {
 		payment := models.Payment{
-			Amount:  paymentReq.Amount,
-			Success: false,
+			Name:             paymentReq.Name,
+			CreditCardNumber: paymentReq.CreditCardNumber,
+			ExpirationDate:   paymentReq.ExpirationDate,
+			CVC:              paymentReq.CVC,
+			Success:          false,
 		}
 		return c.JSON(http.StatusBadRequest, payment)
 	}
 
 	payment := models.Payment{
-		Amount:  paymentReq.Amount,
-		Success: true,
+		Name:             paymentReq.Name,
+		CreditCardNumber: paymentReq.CreditCardNumber,
+		ExpirationDate:   paymentReq.ExpirationDate,
+		CVC:              paymentReq.CVC,
+		Success:          true,
 	}
 
 	return c.JSON(http.StatusOK, payment)
--- Frontend/backend/models/payment.go
+++ Frontend/backend/models/payment.go
@@ -1,10 +1,9 @@
 package models
 
 type Payment struct {
-	Amount           float32 `json:"amount"`
-	Name             string  `json:"name"`
-	CreditCardNumber string  `json:"credit_card_number"`
-	ExpirationDate   string  `json:"expiration_date"`
-	CVC              uint16  `json:"cvc"`
-	Success          bool    `json:"success"`
+	Name             string `json:"name"`
+	CreditCardNumber string `json:"credit_card_number"`
+	ExpirationDate   string `json:"expiration_date"`
+	CVC              uint16 `json:"cvc"`
+	Success          bool   `json:"success"`
 }
